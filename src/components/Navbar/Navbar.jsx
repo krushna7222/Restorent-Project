6361a380482cd6ab5960ca9bfbde97728f4ad33e
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-25 23:22:49 +0530

    Created layout of Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
+  <div id="navbar">
+    <div id="image">
+      <img src="src\assets\navlogo.png" alt="" />
+    </div>
+    <div id="menuitems">
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#menu">Menu</a>
+        </li>
+        <li>
+          <a href="#awards">Awards</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+    <div id="options">
+      <ul>
+        <li>
+          <a href="#login">Login/Register /</a>
+        </li>
+        <li>
+          <a href="#booktable">Book Table</a>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
