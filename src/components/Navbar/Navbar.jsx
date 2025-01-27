599eb1af6a99d29deb05bfeae7fa787a8960c4e9
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-27 14:19:55 +0530

    In Navbar file menu bar is added for mobile friendlya view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,77 @@
 import React from "react";
-
 import "./Navbar.css";
 
-const Navbar = () => (
-  <div id="navbar">
-    <div id="image">
-      <img src="src\assets\navlogo.png" alt="" />
-    </div>
-    <div id="menuitems">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#menu">Menu</a>
-        </li>
-        <li>
-          <a href="#awards">Awards</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-    <div id="options">
-      <ul>
-        <li>
-          <a href="#login">Login/Register /</a>
-        </li>
-        <li>
-          <a href="#booktable">Book Table</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+const Navbar = () => {
+  return (
+    <>
+      <div id="navbar">
+        <div id="image">
+          <img src="src\assets\navlogo.png" alt="Logo" />
+        </div>
+        <div id="menuitems">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#menu">Menu</a>
+            </li>
+            <li>
+              <a href="#awards">Awards</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div id="options">
+          <ul>
+            <li>
+              <a href="#login">Login/Register /</a>
+            </li>
+            <li>
+              <a href="#booktable">Book Table</a>
+            </li>
+          </ul>
+        </div>
+        <div className="rightmenu">
+          <ul>
+            <li>
+              <a href="" id="menu-item">
+                <i class="fa-solid fa-bars"></i>
+              </a>
+              <ul className="megamenu" id="mega-menu">
+                <li>
+                  <a href="#about">Home</a>
+                </li>
+                <li>
+                  <a href="#skills">About</a>
+                </li>
+                <li>
+                  <a href="#projects">Menu</a>
+                </li>
+                <li>
+                  <a href="#cources">Awards</a>
+                </li>
+                <li>
+                  <a href="#experiance">Contact</a>
+                </li>
+                <li>
+                  <a href="#contact">Login/Register</a>
+                </li>
+                <li>
+                  <a href="#contact">Book Table</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
