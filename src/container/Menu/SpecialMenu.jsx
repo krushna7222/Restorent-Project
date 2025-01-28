972f7e4f754d93b4de6c27d4be9d7e2fc5e76f07
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-28 14:29:36 +0530

    Menu List layout created

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import "./SpecialMenu.css";
 
-import './SpecialMenu.css';
-
-const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
-);
+const SpecialMenu = () => {
+  return (
+    <>
+      <section id="menu">
+        <div id="top">
+          <div>
+            <h3 id="menumsg">Menu that fits you palatte</h3>
+            <p align="center">
+              <img src="src\assets\spoon.svg" alt="Spoon" />
+            </p>
+            <h1 align="center" id="menuid">
+              Today's Special
+            </h1>
+          </div>
+        </div>
+        <div id="menu-container">
+          <div id="winebeer">
+            <h2>Wine & Beer</h2>
+          </div>
+          <div id="menuimg">
+            <img src="src\assets\menu.png" alt="Menu Image" />
+          </div>
+          <div id="cocktail">
+            <h2>Cocktails</h2>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default SpecialMenu;
