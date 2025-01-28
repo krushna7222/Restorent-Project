4688f00d1ee37f424d50e5d7654b0f315e42d04b
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-29 00:08:29 +0530

    for intro section video source is updated

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Intro.css';
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  return (
+    <section id="intro-video">
+      <video muted loop controls>
+        <source src="src\assets\meal.mp4" type="video/mp4" />
+      </video>
+    </section>
+  );
+};
 
 export default Intro;
