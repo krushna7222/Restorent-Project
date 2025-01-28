b414ecbbf08ce1bd6e0e7a5aa21c9e7d5e199044
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-28 23:28:57 +0530

    Chef Layout is created Proper Responsive for all devices

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import "./Chef.css";
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+const Chef = () => {
+  return (
+    <section id="chef">
+      <div id="chef-image">
+        <img src="src\assets\chef.png" alt="chef" />
+      </div>
+      <div id="chef-text">
+        <p>Chef's Word</p>
+        <p>
+          <img src="src\assets\spoon.svg" alt="Spoon" />
+        </p>
+        <h1>What we believe in</h1>
+        <p>
+          <img src="src\assets\quote.png" alt="qoute" /> A chef is a skilled
+          culinary professional responsible for preparing, cooking, and
+          presenting food in a professional kitchen. Chefs often specialize in
+          particular cuisines or types of dishes and work in restaurants,
+          hotels, catering services, or other food establishments. Their primary
+          responsibilities include creating menus, sourcing and inspecting
+          ingredients, supervising kitchen staff, and ensuring food safety and
+          quality standards are met.
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Chef;
