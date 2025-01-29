d69ff7598798efe50fa23aa92cec11eb51ef1527
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-30 00:31:49 +0530

    created image gallery and description section layoit

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import './Gallery.css';
+import "./Gallery.css";
+import RestaurantGallery from "./RestaurantGallery";
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  return (
+    <section id="gallary">
+      <div id="gallary-description">
+        <p>Instagram</p>
+        <p>
+          <img src="src\assets\spoon.svg" alt="Spoon" />
+        </p>
+        <h1>Photo Gallery</h1>
+        <h4>"Welcome to Our Culinary Wonderland!"</h4>
+        <p>
+          Step into our restaurant through this exquisite photo gallery. Explore
+          the artistry and ambiance that make dining with us an unforgettable
+          experience. From our chef-crafted dishes bursting with flavor to the
+          warm and inviting interiors designed for comfort and elegance, every
+          detail reflects our passion for hospitality.
+        </p>
+        <button>View More</button>
+      </div>
+      <div id="image-gallary">
+        <RestaurantGallery></RestaurantGallery>
+      </div>
+    </section>
+  );
+};
 
 export default Gallery;
