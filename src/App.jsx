0cf035e89f99fb33bfd1a98a694db720a8d2759e
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-02-01 01:03:14 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import {
   AboutUs,
   Chef,
@@ -14,21 +15,34 @@ import {
 } from "./container";
 import { Navbar } from "./components";
 import "./App.css";
+import Login from "./container/Login/Login";
+import Register from "./container/Register/Register";
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Subscribe />
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Subscribe />
+      <Footer />
+
+      {/* <div>
+        {isLogin ? (
+          <Login onSwitch={() => setIsLogin(false)} />
+        ) : (
+          <Register onSwitch={() => setIsLogin(true)} />
+        )}
+      </div> */}
+    </>
+  );
+};
 
 export default App;
