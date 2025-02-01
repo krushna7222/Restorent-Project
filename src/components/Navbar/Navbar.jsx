7485b316eda2bed541679d4d12435bdd54303686
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-02-01 12:06:14 +0530

    updated navbar for rout and onclick megamenu display

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,70 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false); // Hide the menu when a navigation item is clicked
+  };
+
   return (
     <>
       <div id="navbar">
         <div id="image">
           <img id="navimg" src="navlogo.png" alt="Logo" />
         </div>
-        <div id="menuitems">
+        <div id="menuitems" className={isMenuOpen ? "open-menu" : ""}>
           <ul>
             <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#menu">Menu</a>
-            </li>
-            <li>
-              <a href="#laurels">Awards</a>
-            </li>
-            <li>
-              <a href="#find-us">Contact</a>
-            </li>
-          </ul>
-        </div>
-        <div id="options">
-          <ul>
-            <li>
-              <Link to="/login">Login / Register &nbsp;|</Link>
-            </li>
-            <li>
-              <Link to="/booktable">Book Table</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="rightmenu">
-          <ul>
-            <li>
-              <a href="" id="menu-item">
-                <i class="fa-solid fa-bars"></i>
+              <a href="#home" onClick={closeMenu}>
+                Home
               </a>
-              <ul className="megamenu" id="mega-menu">
+            </li>
+            <li>
+              <a href="#about" onClick={closeMenu}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#menu" onClick={closeMenu}>
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="#laurels" onClick={closeMenu}>
+                Awards
+              </a>
+            </li>
+            <li>
+              <a href="#find-us" onClick={closeMenu}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="options" className={isMenuOpen ? "open-menu" : ""}>
+          <ul>
+            <li>
+              <Link to="/login" onClick={closeMenu}>
+                Login / Register &nbsp;|
+              </Link>
+            </li>
+            <li>
+              <Link to="/booktable" onClick={closeMenu}>
+                Book Table
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="rightmenu" onClick={toggleMenu}>
+          <ul>
+            <li>
+              <a id="menu-item">
+                <i className="fa-solid fa-bars"></i>
+              </a>
+              <ul className={`megamenu ${isMenuOpen ? "open-megamenu" : ""}`}>
                 <li>
-                  <a href="#home">Home</a>
+                  <a href="#home" onClick={closeMenu}>
+                    Home
+                  </a>
                 </li>
                 <li>
-                  <a href="#about">About</a>
+                  <a href="#about" onClick={closeMenu}>
+                    About
+                  </a>
                 </li>
                 <li>
-                  <a href="#menu">Menu</a>
+                  <a href="#menu" onClick={closeMenu}>
+                    Menu
+                  </a>
                 </li>
                 <li>
-                  <a href="#laurels">Awards</a>
+                  <a href="#laurels" onClick={closeMenu}>
+                    Awards
+                  </a>
                 </li>
                 <li>
-                  <a href="#find-us">Contact</a>
+                  <a href="#find-us" onClick={closeMenu}>
+                    Contact
+                  </a>
                 </li>
                 <li>
-                  <Link to="/login">Login / Register &nbsp;|</Link>
+                  <Link to="/login" onClick={closeMenu}>
+                    Login / Register
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/booktable">Book Table</Link>
+                  <Link to="/booktable" onClick={closeMenu}>
+                    Book Table
+                  </Link>
                 </li>
               </ul>
             </li>
