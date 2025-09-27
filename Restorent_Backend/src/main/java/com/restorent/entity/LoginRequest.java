68e37d69cd99a0800b4801ce403f45e69cd5122b
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-09-27 22:51:51 +0530

    Login And Register Request

---
+++ Restorent_Backend/src/main/java/com/restorent/entity/LoginRequest.java
@@ -0,0 +1,6 @@
+package com.restorent.entity;
+
+public class LoginRequest {
+	public String email;
+	public String password;
+}
---
+++ Restorent_Backend/src/main/java/com/restorent/entity/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.restorent.entity;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+	public String name;
+	public String email;
+	public String password;
+	
+}
