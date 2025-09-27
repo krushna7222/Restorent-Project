52bf8e482e44b9d1e2cdaa7dab637e3a83ede348
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-09-27 22:54:17 +0530

    Spring Boot Application

---
+++ Restorent_Backend/src/main/java/com/restorent/RestorentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.restorent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorentBackendApplication.class, args);
+	}
+
+}
