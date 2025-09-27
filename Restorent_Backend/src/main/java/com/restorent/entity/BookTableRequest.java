98714853aaa094ba16adbe0cadb131353d36e0df
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-09-28 00:01:14 +0530

    Update BookTableRequest.java

--- Restorent_Backend/src/main/java/com/restorent/entity/BookTableRequest.java
+++ Restorent_Backend/src/main/java/com/restorent/entity/BookTableRequest.java
@@ -15,22 +15,4 @@ public class BookTableRequest {
     public LocalTime time; 
     public Integer guests; 
 
-    // getters and setters
-//    public String getName() { return name; }
-//    public void setName(String name) { this.name = name; }
-//
-//    public String getEmail() { return email; }
-//    public void setEmail(String email) { this.email = email; }
-//
-//    public String getPhone() { return phone; }
-//    public void setPhone(String phone) { this.phone = phone; }
-//
-//    public LocalDate getDate() { return date; }
-//    public void setDate(LocalDate date) { this.date = date; }
-//
-//    public LocalTime getTime() { return time; }
-//    public void setTime(LocalTime time) { this.time = time; }
-//
-//    public Integer getGuests() { return guests; }
-//    public void setGuests(Integer guests) { this.guests = guests; }
 }
