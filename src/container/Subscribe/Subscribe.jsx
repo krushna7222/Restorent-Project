ecda6568da984372791fa2784e7141057e07c1c9
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-10-05 17:39:50 +0530

    done subscribe functionality

--- src/container/Subscribe/Subscribe.jsx
+++ src/container/Subscribe/Subscribe.jsx
@@ -1,7 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./Subscribe.css";
+import { toast } from "react-toastify";
+
 const Subscribe = () => {
+  const [email, setEmail] = useState("");
+
+  const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${BASE_URL}/api/v1/subscribe`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (!response.ok) {
+        // console.log(response.data);
+        throw new Error(response.data);
+      }
+
+      const data = await response.json();
+      console.log("✅ Suscribed:", data);
+      toast.success("Suscribed Successful!");
+    } catch (error) {
+      console.error("❌ Error during Suscribe:", error);
+      toast.error("Suscribe Failed!");
+    }
+  };
+
   return (
     <section id="subscribe">
       <div>
@@ -9,10 +40,17 @@ const Subscribe = () => {
         <p>And Never Miss Latest Updates!</p>
         <form>
           <div>
-            <input type="email" placeholder="Enter Your Email" />
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
-            <button type="submit">Subscribe</button>
+            <button type="submit" onClick={handleSubmit}>
+              Subscribe
+            </button>
           </div>
         </form>
       </div>
