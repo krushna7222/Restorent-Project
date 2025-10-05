1afa321c1544855d2b8172d9f0c3fabc211c4e7e
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-10-05 17:38:07 +0530

    Update BookingTablesRepository.java

--- Restorent_Backend/src/main/java/com/restorent/repository/BookingTablesRepository.java
+++ Restorent_Backend/src/main/java/com/restorent/repository/BookingTablesRepository.java
@@ -1,9 +1,12 @@
 package com.restorent.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.restorent.entity.BookingTables;
 
 public interface BookingTablesRepository extends JpaRepository<BookingTables, Integer> {
+    List<BookingTables> findByEmail(String email);
 
 }
